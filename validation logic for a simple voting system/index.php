8cb2822c21d0712d0dc8356ceeafecb5fe408467
ilechukwu joshua <ilechukwujosh11@gmail.com>
2023-03-18 05:35:21 -0700

    Validation login of voter system

---
+++ validation logic for a simple voting system/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$voterAge = 18;
+$voterPvc = True;
+$voterWard = 020;
+
+    if($voterAge === 18){
+        if($voterPvc === True){
+            if ($voterWard == 020){
+                echo "Voter eligible to vote";
+            }else{
+                echo "This is not your pulling unit";
+            }
+        }else{
+            echo "You can't vote without a PVC";
+        }
+    }else{
+        echo "You are not Eligible to vote";
+    }
+?>
